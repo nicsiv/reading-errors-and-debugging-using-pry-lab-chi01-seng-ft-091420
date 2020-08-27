6a255aad524b2154a1a19b3d010bc2c1da471228
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-27 18:59:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+require 'pry'# don't forget to add: require 'pry'
 
-def snake_it_up(string)
+def snake_it_up("string")
   if string[0] == "s"
-    10 * "s" + string
+    10 * "s" + "string"
   else
     string
+    binding.pry
   end
 end
